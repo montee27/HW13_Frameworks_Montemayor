b1f876bb9cd7847d20cdc7867a6764d0306f6ece
Alex Montemayor <amontemayor@sandiego.edu>
2026-04-26 20:51:29 -0700

    fix: removed imports

--- src/main/java/edu/sandiego/comp305/demo/DemoApplication.java
+++ src/main/java/edu/sandiego/comp305/demo/DemoApplication.java
@@ -2,10 +2,6 @@ package edu.sandiego.comp305.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.Arrays;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
