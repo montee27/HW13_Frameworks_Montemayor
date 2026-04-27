26aba39a9d85157695548baeda74d98316b231b2
Alex Montemayor <amontemayor@sandiego.edu>
2026-04-26 20:30:23 -0700

    feat: added imports to DemoApplication

--- src/main/java/edu/sandiego/comp305/demo/DemoApplication.java
+++ src/main/java/edu/sandiego/comp305/demo/DemoApplication.java
@@ -2,6 +2,11 @@ package edu.sandiego.comp305.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Arrays;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class DemoApplication {
